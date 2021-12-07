356260dac12bdd6178ab28a11b879775a2e87048
hmfayyaz <36409793+hmfayyaz@users.noreply.github.com>
2021-12-07 22:18:28 +0500

    package name changed.

--- android/app/src/main/java/com/app/myapartment/MainActivity.java
+++ android/app/src/main/java/com/app/myapartment/MainActivity.java
@@ -1,4 +1,4 @@
-package com.yourhotelapp;
+package com.app.myapartment;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "YourHotelApp";
+    return "MyApartment";
   }
 }
--- android/app/src/main/java/com/app/myapartment/MainApplication.java
+++ android/app/src/main/java/com/app/myapartment/MainApplication.java
@@ -1,4 +1,4 @@
-package com.yourhotelapp;
+package com.app.myapartment;
 
 import android.app.Application;
 import android.content.Context;
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.yourhotelapp.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.app.myapartment.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
